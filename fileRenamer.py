9a00a7afc014b2ce2cba7e0c0bf398227df7491e
CuV-Dev <58642374+CuV-Dev@users.noreply.github.com>
2021-12-09 22:04:02 +0100

    Added the creation of a Sandbox with 1000 files

--- fileRenamer.py
+++ fileRenamer.py
@@ -1,5 +1,41 @@
+import os
+import glob
+
+
 def main():
-    print('Hello World!')
+    """
+    The Main function of this program.
+
+    :return: void
+    """
+    renameFiles()
+#    createSandbox()
+
+
+def renameFiles():
+    """
+    The Core functionality of this program
+
+    :return: void
+    """
+    pass
+
+
+def createSandbox():
+    """
+    Creates a Sandbox with 1000 txt files.
+
+    :return: void
+    """
+    print('Started setting up Sandbox...')
+    print('Started clearing the Sandbox directory...')
+    files = glob.glob('/Sandbox/*')
+    for file in files:
+        os.remove(file)
+    print('Successfully cleared Sandbox directory!')
+    for x in range(0, 1000):
+        open('./Sandbox/myData' + str(x) + '.txt', 'w')
+    print('Sandbox successfully set up!')
 
 
 if __name__ == "__main__":
